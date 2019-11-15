71b18f08ab4df8e9d75cbea40aef758823e61a1a
sarah <s.j.condon01@gmail.com>
2019-11-15 22:12:38 +0000

    Automatically backed up by Learn

--- lib/schema.sql
+++ lib/schema.sql
@@ -1,38 +1,38 @@
-CREATE TABLE Series (
+CREATE TABLE series (
   id INTEGER PRIMARY KEY,
   title TEXT,
   author_id INTEGER,
   subgenre_id INTEGER
 );
 
-CREATE TABLE SubGenres (
+CREATE TABLE subgenres (
   id INTEGER PRIMARY KEY,
   name TEXT
 );
 
-CREATE TABLE Authors (
+CREATE TABLE authors (
   id INTEGER PRIMARY KEY,
   name TEXT
 );
 
-CREATE TABLE Books (
+CREATE TABLE books (
   id INTEGER PRIMARY KEY,
   title TEXT,
   year INTEGER,
   series_id INTEGER
 );
 
-CREATE TABLE Characters (
+CREATE TABLE characters (
   id INTEGER PRIMARY KEY,
   name TEXT,
-  motto TEXT,
   species TEXT,
+  motto TEXT,
   author_id INTEGER,
   series_id INTEGER
 );
 
 CREATE TABLE character_books (
   id INTEGER PRIMARY KEY,
-  character_id INTEGER,
-  book_id INTEGER
+  book_id INTEGER,
+  character_id INTEGER
 );
